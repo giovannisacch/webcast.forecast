7691b955ca6684394aab766ef01d3460ec78ff10
Giovanni Sacchitiello <sacchitiellogiovanni@gmail.com>
2022-04-24 10:13:22 -0300

    Improving repository by adding a "consolidate index"

--- WebCast.Forecast.Infra/WeatherForecastRepository.cs
+++ WebCast.Forecast.Infra/WeatherForecastRepository.cs
@@ -13,6 +13,7 @@ namespace WebCast.Forecast.Infra
     public class WeatherForecastRepository : IWeatherForecastRepository
     {
         private readonly IDistributedCache _repository;
+        private readonly string _aggregateIndexPrefix = "weekAggregate:";
         public WeatherForecastRepository(IDistributedCache repository)
         {
             _repository = repository;
@@ -23,14 +24,27 @@ namespace WebCast.Forecast.Infra
             {
                 AbsoluteExpiration =  weatherForecast.Date.AddTicks(new TimeOnly(23, 59, 59).Ticks)
             };
+
             var weatherForecastSerliazed = JsonSerializer.Serialize(weatherForecast);
 
-            await _repository.SetStringAsync(GetCacheKeyByWheatherForecastDate(weatherForecast.Date) , weatherForecastSerliazed, options);
-        }
+            await _repository.SetStringAsync(GetCacheKeyByWheatherForecastDate(weatherForecast.Date), weatherForecastSerliazed, options);
 
+            if (weatherForecast.Date.CompareTo(DateTime.Now.AddDays(7)) < 0)
+                await UpdateNextSevenDaysAggregateIndex();
+        }
         public async Task<IEnumerable<WeatherForecast>> GetNextSevenDaysWeatherForecastsAsync()
         {
-            var weatherForecasts = new WeatherForecast[7];
+            var weatherForecastAggregate = await _repository.GetStringAsync(_aggregateIndexPrefix + GetCacheKeyByWheatherForecastDate(DateTime.Now.Date));
+            if (string.IsNullOrEmpty(weatherForecastAggregate))
+                return await UpdateNextSevenDaysAggregateIndex();
+
+            var result = JsonSerializer.Deserialize<IEnumerable<WeatherForecastDTO>>(weatherForecastAggregate);
+            return result.Select(x => new WeatherForecast(x.Temperature, x.Date));
+        }
+
+        private async Task<List<WeatherForecast>> GetWeatherForecastsFromNextSevenDaysIndexes()
+        {
+            var weatherForecasts = new List<WeatherForecast>();
             var nextSevenDaysWeatherForecastsTasks = new Task[7];
             var actualDate = DateTime.Now;
             for (int i = 0; i < 7; i++)
@@ -41,9 +55,26 @@ namespace WebCast.Forecast.Infra
 
             for (int i = 0; i < nextSevenDaysWeatherForecastsTasks.Length; i++)
             {
-                weatherForecasts[i] = ((Task<WeatherForecast>)nextSevenDaysWeatherForecastsTasks[i]).Result;
+                var taskResult = ((Task<WeatherForecast>)nextSevenDaysWeatherForecastsTasks[i]).Result;
+                if (taskResult == null)
+                    continue;
+                weatherForecasts.Add(taskResult);
             }
-            return weatherForecasts;
+            return weatherForecasts.OrderBy(x => x.Date).ToList();
+        }
+        private async Task<IEnumerable<WeatherForecast>> UpdateNextSevenDaysAggregateIndex()
+        {
+            var actualDate = DateTime.Now.Date;
+            var nextSevenDaysIndexes = await GetWeatherForecastsFromNextSevenDaysIndexes();
+            if ( !nextSevenDaysIndexes.Exists(x => x != null) )
+                return Enumerable.Empty<WeatherForecast>();
+            var options = new DistributedCacheEntryOptions()
+            {
+                AbsoluteExpiration = actualDate.Date.AddTicks(new TimeOnly(23, 59, 59).Ticks)
+            };
+            var weatherForecastsSerliazed = JsonSerializer.Serialize(nextSevenDaysIndexes);
+            await _repository.SetStringAsync(_aggregateIndexPrefix + GetCacheKeyByWheatherForecastDate(actualDate), weatherForecastsSerliazed, options);
+            return nextSevenDaysIndexes;
         }
         private async Task<WeatherForecast> GetWeatherForecastAsync(DateTime date)
         {
